df7f720f4b19803d459ecd32bd071f37c23db749
jaicedev <jaicedev@gmail.com>
2020-08-10 12:33:40 -0400

    Basic Funcionality Done

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function getPublicRepos(){
     $('#js-submit').on('click', function (e){
         e.preventDefault()
+        $('#repo-list').html('');
         let handle = $('#js-handle').val();
-        let repoList = fetch(`api.github.com/users/${handle}/repos`).then(response => response.json())
-        console.log(repoList)
+        $('#js-handle').val('')
+        fetch(`https://api.github.com/users/${handle}/repos`)
+            .then(response => response.json())
+            .then(responseJson => {
+                $('#repo-list').append(buildRepoObjects(responseJson));
+            })
     });
 }
 
+function buildRepoObjects(object){
+    for(i = 0; i < object.length; i++){
+        let repoObject = object[i];
+        console.log(repoObject)
+        let repoName = repoObject.name
+        repoName.toUpperCase();
+        $('#repo-list').append(`
+        <li class="repo-object" id="js-repo-object">` + 
+        `<h1 class="repo-title">${repoName}</h1>` + 
+        `<h2 class="repo-description">${repoObject.description}</h2>` + 
+        `<a href="${repoObject.html_url}">Visit Repo</a>` + 
+        `</li>`);
+    }
+}
 
 $(getPublicRepos)
